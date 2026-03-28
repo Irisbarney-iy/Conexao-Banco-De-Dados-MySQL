557d30fa2a2ba712922ba53b8690dcdc6a6ce2a4
irisbarney damasceno martins filho <irisbarney.iy@gmail.com>
2026-03-28 14:38:30 -0300

    adicionando comentario no metodo de exclusao

--- src/java_Connection/Main.java
+++ src/java_Connection/Main.java
@@ -4,12 +4,21 @@ public class Main {
 	public static void main(String[] args) {
 	    FuncionarioDAO dao = new FuncionarioDAO();
 
+	    Funcionario f = new Funcionario();
+	    f.nome = "Irisbarney";
+	    f.sobrenome = "Damasceno";
+	    f.idade = 17;
+	    f.salario = 9000.0;
+	    dao.inserir(f);
+
 	    System.out.println("Lista após inserção:");
 	    dao.listarTodos();
 
-
-
-	    // 4. TESTE EXCLUIR
+	    f.codigo = 1; 
+	    f.salario = 5500.0;
+	    dao.atualizar(f);
+	    
+	    // Esse comando é para exluir, por isso está como comentário
 	    // dao.excluir(1); 
 	}
 }
